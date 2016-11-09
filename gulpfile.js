bc5d1a6c5b1a7bcc32d704fcdeb353410fe9ebc0
Greg Mendez <Greg Mendez>
2016-11-09 11:52:34 -0500

    updated for workers So much better

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,23 @@ var typescript = require('gulp-tsc');
 var concat = require('gulp-concat');
 var template = require('gulp-template');
 var uglify = require('gulp-uglify');    
-inject = require('gulp-inject-string');
-
+var inject = require('gulp-inject-string');
+var tap = require('gulp-tap');
+var insert = require('gulp-insert');
+var path = require('path');
 gulp.task('default', () =>
     gulp.src('src/Workers/*.ts')
         .pipe(typescript())
         .pipe(uglify())
-        .pipe(inject.prepend('//THIS is a Test Created:'+Date()+'\n') )
+        
+        .pipe(insert.transform(function(contents, file) {
+            var comment = path.basename(file.path).substring(0,(path.basename(file.path).length-3))+"={js:`";
+            return comment + contents;
+        }))
+        .pipe(inject.prepend("Worker."))
+        
+        .pipe(inject.append("`};"))
         .pipe(concat('all.min.js'))
+        .pipe(inject.prepend('var Worker;'))
         .pipe(gulp.dest('src/Workers'))
 );
