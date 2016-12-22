8c79b1d479e693a2a4f3a50e546a6364dbd7db42
Greg <gregmen@hotmail.com>
2016-12-22 11:30:13 -0500

    Update gulpfile.js
    updated web worker process in another application bringing it here

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,31 @@ var gulp = require('gulp');
 var typescript = require('gulp-tsc');
 var concat = require('gulp-concat');
 var template = require('gulp-template');
-var uglify = require('gulp-uglify');    
+var uglify = require('gulp-uglify');
 var inject = require('gulp-inject-string');
 var tap = require('gulp-tap');
 var insert = require('gulp-insert');
 var path = require('path');
-gulp.task('default', () =>
-    gulp.src('src/Workers/*.ts')
+var clean = require('gulp-clean');
+
+
+gulp.task('updateWorker',['clean'], () =>{
+   gulp.src('src/Workers/*.ts')
         .pipe(typescript())
         .pipe(uglify())
-        
-        .pipe(insert.transform(function(contents, file) {
-            var comment = path.basename(file.path).substring(0,(path.basename(file.path).length-3))+"={js:`";
+        .pipe(insert.transform(function (contents, file) {
+            var comment = path.basename(file.path).substring(0, (path.basename(file.path).length - 3)) + "={js:`";
             return comment + contents;
         }))
-        .pipe(inject.prepend("Worker."))
-        
+        .pipe(inject.prepend("public "))
         .pipe(inject.append("`};"))
-        .pipe(concat('all.min.js'))
-        .pipe(inject.prepend('var Worker;'))
-        .pipe(gulp.dest('src/Workers'))
+        .pipe(concat('workers.service.ts'))
+        .pipe(inject.prepend(`import { Injectable } from '@angular/core'; @Injectable() export class WorkersService {  constructor() { }`))
+        .pipe(inject.append('};'))
+        .pipe(gulp.dest('src/app/services'))
+}
 );
+gulp.task('clean',()=>{
+    return gulp.src('src/app/services/workers.service.ts')
+    .pipe(clean());
+});
